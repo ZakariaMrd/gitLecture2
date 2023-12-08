92089b24dae697dec2a3cb515bd15a6f9e3f9aa0
MOURID Zakaria <zakariamourid142@gmail.com>
2023-12-08 11:37:06 +0100

    first commit with new repo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Version final remote ..");
+        System.out.println("Hello world! It's the new repository");
     }
 }
