46ba30ce851e71837ee141b27169cd5fe871ce46
MOURID Zakaria <zakariamourid142@gmail.com>
2023-12-08 11:06:32 +0100

    final upd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Version final");
+        System.out.println("Hello world! Version final remote");
     }
 }
