f58c30a6b8a7378bb2ff441f3d80f82027c12a44
MOURID Zakaria <zakariamourid142@gmail.com>
2023-12-08 11:24:23 +0100

    2 points

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Version final remote");
+        System.out.println("Hello world! Version final remote .");
     }
 }
